7359aef64aa72bd220b3774ad087ed5ca432406e
LeKazo <70152390+LeKazo@users.noreply.github.com>
2020-12-02 12:13:53 -0500

    Lab1Part2
    :)

---
+++ lab2.js
@@ -0,0 +1,23 @@
+var numberOfApples = 10;
+document.write("the number of apples i have is " + numberOfApples + "<br>");
+numberOfApples++;
+document.write("the number of apples i have is " + numberOfApples + "<br>");
+numberOfApples++;
+document.write("the number of apples i have is " + numberOfApples + "<br>");
+numberOfApples++;
+document.write("the number of apples i have is " + numberOfApples + "<br>");
+numberOfApples++;
+document.write("the number of apples i have is " + numberOfApples + "<br>");
+numberOfApples++;
+document.write("the number of apples i have is " + numberOfApples + "<br>");
+numberOfApples++;
+
+document.write("<br><br>")
+
+var numberOfNinja = 100;
+document.write("there are "+ --numberOfNinja + "ninjas in said room. <br>")
+document.write("there are "+ --numberOfNinja + "ninjas in said room. <br>")
+document.write("there are "+ --numberOfNinja + "ninjas in said room. <br>")
+document.write("there are "+ --numberOfNinja + "ninjas in said room. <br>")
+document.write("there are "+ --numberOfNinja + "ninjas in said room. <br>")
+document.write("Created by Q")
